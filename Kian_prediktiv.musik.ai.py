5dca0bfd59cd1909bff806ded89f1ec074a3272d
kianbigdeli-gif <kian.bigdeli@elev.ga.lbs.se>
2026-04-23 14:18:36 +0200

    Add files via upload

---
+++ Kian_prediktiv.musik.ai.py
@@ -0,0 +1,15 @@
+media =[
+    {"artist": "Bad Bunny", "Genre": "Latin Trap", "Ranking": "1", "Age": "32"}
+    {"artist": "Taylor Swift", "Genre": "pop", "Ranking": "1", "Age": "32"}
+    {"artist": "The Weeknd", "Genre": "R&B", "Ranking": "1", "Age": "32"}
+    {"artist": "Drake", "Genre": "Rap", "Ranking": "1", "Age": "32"}
+    {"artist": "Billie Eilish", "Genre": "Alternative pop", "Ranking": "1", "Age": "32"}
+    {"artist": "Kendrick Lamar", "Genre": "Hip-Hop", "Ranking": "1", "Age": "32"}
+    {"artist": "Bruno Mars", "Genre": "Pop", "Ranking": "1", "Age": "32"}
+    {"artist": "Ariana Grande", "Genre": "Pop", "Ranking": "1", "Age": "32"}
+    {"artist": "Arijit Singh" "Genre": "Bollywood", "Ranking": "1", "Age": "32"}
+    {"artist": "Fuerza Regida", "Genre": "Regional mexican", "Ranking": "1", "Age": "32"}]
+
+genre = input(" What Genre do you enjoy? ")
+mood = input(" What type of mood do you want?")
+
